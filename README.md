47075304340ba8c91485d3e12ff75932496d8bf3
root <root@ip-172-31-51-88.ec2.internal>
2016-09-07 00:31:59 +0000

    drupalbuild site

---
+++ README.md
@@ -0,0 +1,1 @@
+# drupalbuild
---
+++ sites/all/modules/civicrm/civicrm-version.php
@@ -0,0 +1,7 @@
+<?php
+function civicrmVersion( ) {
+  return array( 'version'  => '4.7.10',
+                'cms'      => 'Drupal',
+                'revision' => 'e5fcc24b37' );
+}
+
